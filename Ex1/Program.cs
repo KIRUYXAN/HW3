dc28f551a115a19623215b93cab48fc9e2a4be67
KIRUYXAN <279901@gmail.com>
2021-12-19 01:04:31 +0300

    .

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,2 +1,68 @@
-п»ї// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//а) Дописать структуру Complex, добавив метод вычитания комплексных чисел.
+//Продемонстрировать работу структуры.
+//б) Дописать класс Complex, добавив методы вычитания и произведения чисел.
+//Проверить работу класса.
+//в) Добавить диалог с использованием switch демонстрирующий работу класс.
+
+Console.WriteLine("Struct or Class");
+string a = Console.ReadLine();
+if (a == "Struct" || a == "struct")
+{
+	ComplexS z1 = new ComplexS(1, 2);
+	Console.WriteLine("Struct: " + z1.Print());
+
+	ComplexS z2 = new ComplexS(4, -6);
+	Console.WriteLine("Struct: " + z2.Print());
+
+	ComplexS z3;
+	z3 = z1.Sum(z2);
+	Console.WriteLine("StructSum: " + z3.Print());
+	z3 = z1 + z2;
+	Console.WriteLine("StructSum: " + z3.Print());
+
+	ComplexS z4;
+	z4 = z1 - z2;
+	Console.WriteLine("StructSub: " + z4.Print());
+}
+
+Console.WriteLine("");
+
+if (a == "Class" || a == "class")
+{
+	ComplexC x1 = new ComplexC(1, 2);
+	Console.WriteLine("Class: " + x1.Print());
+
+	ComplexC x2 = new ComplexC(4, -6);
+	Console.WriteLine("Class: " + x2.Print());
+
+	Console.WriteLine("Plus or Minus or Mult");
+	string act = Console.ReadLine();
+	int g = 0;
+	if (act == "Plus" || act == "plus")
+		g = 1;
+	else if (act == "Minus" || act == "minus")
+		g = 2;
+	else if (act == "Mult" || act == "mult")
+		g = 3;
+
+	switch (g)
+	{
+		case 1:
+			ComplexC x3;
+			x3 = x1.Sum(x2);
+			Console.WriteLine("ClassSum: " + x3.Print());
+			x3 = x1 + x2;
+			Console.WriteLine("ClassSum: " + x3.Print());
+			break;
+		case 2:
+			ComplexC x4;
+			x4 = x1 - x2;
+			Console.WriteLine("ClassSub: " + x4.Print());
+			break;
+		case 3:
+			ComplexC x5;
+			x5 = x1 * x2;
+			Console.WriteLine("ClassMult: " + x5.Print());
+			break;
+	}
+}
---
+++ Ex1/ProgramSC.cs
@@ -0,0 +1,75 @@
+﻿struct ComplexS
+{
+    public double re, im;
+    public ComplexS(double a, double b)
+    {
+        re = a;
+        im = b;
+    }
+    public string Print()
+    {
+        string text;
+        if (im < 0) text = $"{re} - {-im}i";
+        else text = $"{re} + {im}i";
+        return text;
+    }
+    public ComplexS Sum(ComplexS z)
+    {
+        ComplexS result;
+        result = new(re + z.re, im + z.im);
+        return result;
+    }
+    public static ComplexS operator +(ComplexS z1, ComplexS z2)
+    {
+        ComplexS result;
+        result = new(z1.re + z2.re, z1.im + z2.im);
+        return result;
+    }
+    public static ComplexS operator -(ComplexS z1, ComplexS z2)
+    {
+        ComplexS result;
+        result = new(z1.re - z2.re, z1.im - z2.im);
+        return result;
+    }
+}
+
+class ComplexC
+{
+    public double re, im;
+    public ComplexC(double a, double b)
+    {
+        re = a;
+        im = b;
+    }
+    public string Print()
+    {
+        string text;
+        if (im < 0)  text = $"{re} - {-im}i";
+        else text = $"{re} + {im}i";
+        return text;
+    }
+    public ComplexC Sum(ComplexC z)
+    {
+        ComplexC result;
+        result = new(re + z.re, im + z.im);
+        return result;
+    }
+    public static ComplexC operator +(ComplexC z1, ComplexC z2)
+    {
+        ComplexC result;
+        result = new(z1.re + z2.re, z1.im + z2.im);
+        return result;
+    }
+    public static ComplexC operator -(ComplexC z1, ComplexC z2)
+    {
+        ComplexC result;
+        result = new(z1.re - z2.re, z1.im - z2.im);
+        return result;
+    }
+    public static ComplexC operator *(ComplexC z1, ComplexC z2)
+    {
+        ComplexC result;
+        result = new((z1.re*z2.re)-(z1.im*z2.im), (z1.re*z2.im)+(z1.im*z2.re));
+        return result;
+    }
+}
