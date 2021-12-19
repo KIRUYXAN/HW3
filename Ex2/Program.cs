8c4766a26d340053e3c16a6dcc542b9c0edf9783
KIRUYXAN <279901@gmail.com>
2021-12-19 16:44:15 +0300

    .

--- Ex2/Program.cs
+++ Ex2/Program.cs
@@ -2,3 +2,26 @@
 //Требуется подсчитать сумму всех нечётных положительных чисел.
 //Сами числа и сумму вывести на экран, используя tryParse.
 
+int sum = 0;
+int a;
+string text = "";
+string b;
+bool flag;
+
+do
+{
+    do
+    {
+        Console.WriteLine("Введите число. Чтобы закончить введите 0");
+        b = Console.ReadLine();
+        flag = int.TryParse(b, out a);
+        if (!flag)
+            Console.WriteLine("Это не число");
+        Console.WriteLine("");
+    } while (!flag);
+    a = Convert.ToInt32(b);
+    if (a > 0 && a % 2 > 0)
+        text += $"{Convert.ToInt32(a)} ";
+        sum += a;
+} while (a != 0) ;
+Console.WriteLine($"\n{text} \n{sum}");
